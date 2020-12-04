bf9937bf1bff59f6ec41ffb5f9dd46d13898af8d
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2020-12-03 23:26:54 -0600

    Templates

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const { response } = require('express');
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
+
 app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (request, response) => {
+  response.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
+});
 
 const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
-});
-
-app.get("/", (request, response) => {
-  // response.send("Hello Bop");
-  response.render("index");
 });
